b2f1ea097cb1c8e106b1b43612c02b8cf7965400
opilane <opilane@HP-CZC2349HV0>
2026-02-05 12:19:40 +0200

    Add project files.

---
+++ NadisIKTpv25TAR/NaidisFunktsioonid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace NadisIKTpv25TAR
+{
+    internal class NaidisFunktsioonid
+    {
+        public static float Summa(float arv1, float arv2)
+        {
+            return arv1+arv2;
+        }
+    }
+}
---
+++ NadisIKTpv25TAR/StartPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace NadisIKTpv25TAR
+{
+    public class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1. Osa Andmetüübid");
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            string tekst = "123";
+            Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
+            Console.WriteLine("Oli loodud muutja tekst, mis võrdub: {0}", tekst);
+            Console.Write("Mis on sinu nimi?: ");
+            string nimi = Console.ReadLine();
+            Console.Write("Kui vana sa oled?: ");
+            try
+            {
+                int vanus = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e); 
+            }
+            try
+            {
+                Console.Write("Arv 1: ");
+                float a = float.Parse(Console.ReadLine());
+                Console.Write("Arv 2: ");
+                float b = float.Parse(Console.ReadLine());
+                float vastus = NaidisFunktsioonid.Summa(a, b);
+                Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+            }
+            catch (Exception d)
+            {
+                Console.WriteLine(d);
+            }
+
+
+        }
+    }
+}
